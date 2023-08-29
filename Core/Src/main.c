c052cf1c7007864a5e6781133589c9f4663bae39
LiamLetts36 <liam-letts@hotmail.com>
2023-08-29 17:45:30 +0200

    Completed Code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -55,7 +55,8 @@ TIM_HandleTypeDef htim16;
 // TODO: Define any input variables
 static uint8_t patterns[] = {10101010, 01010101, 11001100, 00110011, 11110000, 00001111};
 static uint16_t counter = 0;
-static uint8_t count = 0;
+uint8_t static check = 0;
+
 
 /* USER CODE END PV */
 
@@ -112,7 +113,7 @@ int main(void)
 
   // TODO: Write all "patterns" to EEPROM using SPI
   for(int i = 0; i < sizeof(patterns)/sizeof(patterns[0]); i++){
-	  write_to_address(0x0000 + i, patterns[i]);
+    write_to_address(0x0000 + i, patterns[i]);
   }
 
 
@@ -126,16 +127,22 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
-	// TODO: Check button PA0; if pressed, change timer delay
-	  if((GPIOA->IDR & SW0)==0)
-	  {
+  // TODO: Check button PA0; if pressed, change timer delay
 
-	  }
-	  else
-	  {
-
-	  }
-
+     if(!(LL_GPIO_ReadInputPort(Button0_GPIO_Port) & Button0_Pin))
+      {
+        if(check==0)
+        {
+          htim16.Init.Period = 1000-1;
+          check=1;
+        }
+        else
+        {
+          htim16.Init.Period = 500;
+          check=0;
+        }
+        HAL_TIM_Base_Init(&htim16);
+      }
   }
   /* USER CODE END 3 */
 }
@@ -339,24 +346,24 @@ static void MX_GPIO_Init(void)
 static void init_spi(void) {
 
   // Clock to PB
-  RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 	// Enable clock for SPI port
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN;  // Enable clock for SPI port
 
   // Set pin modes
   GPIOB->MODER |= GPIO_MODER_MODER13_1; // Set pin SCK (PB13) to Alternate Function
   GPIOB->MODER |= GPIO_MODER_MODER14_1; // Set pin MISO (PB14) to Alternate Function
   GPIOB->MODER |= GPIO_MODER_MODER15_1; // Set pin MOSI (PB15) to Alternate Function
   GPIOB->MODER |= GPIO_MODER_MODER12_0; // Set pin CS (PB12) to output push-pull
-  GPIOB->BSRR |= GPIO_BSRR_BS_12; 		// Pull CS high
+  GPIOB->BSRR |= GPIO_BSRR_BS_12;     // Pull CS high
 
   // Clock enable to SPI
   RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
-  SPI2->CR1 |= SPI_CR1_BIDIOE; 									// Enable output
-  SPI2->CR1 |= (SPI_CR1_BR_0 |  SPI_CR1_BR_1); 					// Set Baud to fpclk / 16
-  SPI2->CR1 |= SPI_CR1_MSTR; 									// Set to master mode
-  SPI2->CR2 |= SPI_CR2_FRXTH; 									// Set RX threshold to be 8 bits
-  SPI2->CR2 |= SPI_CR2_SSOE; 									// Enable slave output to work in master mode
-  SPI2->CR2 |= (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2); 	// Set to 8-bit mode
-  SPI2->CR1 |= SPI_CR1_SPE; 									// Enable the SPI peripheral
+  SPI2->CR1 |= SPI_CR1_BIDIOE;                  // Enable output
+  SPI2->CR1 |= (SPI_CR1_BR_0 |  SPI_CR1_BR_1);          // Set Baud to fpclk / 16
+  SPI2->CR1 |= SPI_CR1_MSTR;                  // Set to master mode
+  SPI2->CR2 |= SPI_CR2_FRXTH;                   // Set RX threshold to be 8 bits
+  SPI2->CR2 |= SPI_CR2_SSOE;                  // Enable slave output to work in master mode
+  SPI2->CR2 |= (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2);  // Set to 8-bit mode
+  SPI2->CR1 |= SPI_CR1_SPE;                   // Enable the SPI peripheral
 }
 
 // Implements a delay in microseconds
@@ -372,94 +379,111 @@ static void delay(uint32_t delay_in_us) {
 // Write to EEPROM address using SPI
 static void write_to_address(uint16_t address, uint8_t data) {
 
-	uint8_t dummy; // Junk from the DR
+  uint8_t dummy; // Junk from the DR
 
-	// Set the Write Enable latch
-	GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
-	delay(1);
-	*((uint8_t*)(&SPI2->DR)) = WREN;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
-	dummy = SPI2->DR;
-	GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
-	delay(5000);
+  // Set the Write Enable latch
+  GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
+  delay(1);
+  *((uint8_t*)(&SPI2->DR)) = WREN;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
+  dummy = SPI2->DR;
+  GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+  delay(5000);
 
-	// Send write instruction
-	GPIOB->BSRR |= GPIO_BSRR_BR_12; 			// Pull CS low
-	delay(1);
-	*((uint8_t*)(&SPI2->DR)) = WRITE;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send write instruction
+  GPIOB->BSRR |= GPIO_BSRR_BR_12;       // Pull CS low
+  delay(1);
+  *((uint8_t*)(&SPI2->DR)) = WRITE;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Send 16-bit address
-	*((uint8_t*)(&SPI2->DR)) = (address >> 8); 	// Address MSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
-	*((uint8_t*)(&SPI2->DR)) = (address); 		// Address LSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send 16-bit address
+  *((uint8_t*)(&SPI2->DR)) = (address >> 8);  // Address MSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
+  *((uint8_t*)(&SPI2->DR)) = (address);     // Address LSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Send the data
-	*((uint8_t*)(&SPI2->DR)) = data;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
-	dummy = SPI2->DR;
-	GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
-	delay(5000);
+  // Send the data
+  *((uint8_t*)(&SPI2->DR)) = data;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
+  dummy = SPI2->DR;
+  GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+  delay(5000);
 }
 
 // Read from EEPROM address using SPI
 static uint8_t read_from_address(uint16_t address) {
 
-	uint8_t dummy; // Junk from the DR
+  uint8_t dummy; // Junk from the DR
 
-	// Send the read instruction
-	GPIOB->BSRR |= GPIO_BSRR_BR_12; 			// Pull CS low
-	delay(1);
-	*((uint8_t*)(&SPI2->DR)) = READ;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send the read instruction
+  GPIOB->BSRR |= GPIO_BSRR_BR_12;       // Pull CS low
+  delay(1);
+  *((uint8_t*)(&SPI2->DR)) = READ;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Send 16-bit address
-	*((uint8_t*)(&SPI2->DR)) = (address >> 8); 	// Address MSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0);		// Hang while RX is empty
-	dummy = SPI2->DR;
-	*((uint8_t*)(&SPI2->DR)) = (address); 		// Address LSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send 16-bit address
+  *((uint8_t*)(&SPI2->DR)) = (address >> 8);  // Address MSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
+  *((uint8_t*)(&SPI2->DR)) = (address);     // Address LSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Clock in the data
-	*((uint8_t*)(&SPI2->DR)) = 0x42; 			    // Clock out some junk data
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
-	GPIOB->BSRR |= GPIO_BSRR_BS_12; 			    // Pull CS high
-	delay(5000);
+  // Clock in the data
+  *((uint8_t*)(&SPI2->DR)) = 0x42;          // Clock out some junk data
+  while ((SPI2->SR & SPI_SR_RXNE) == 0);    // Hang while RX is empty
+  dummy = SPI2->DR;
+  GPIOB->BSRR |= GPIO_BSRR_BS_12;           // Pull CS high
+  delay(5000);
 
-	return dummy;								              // Return read data
+  return dummy;                             // Return read data
 }
 
 // Timer rolled over
 void TIM16_IRQHandler(void)
 {
-	// Acknowledge interrupt
-	HAL_TIM_IRQHandler(&htim16);
+  // Acknowledge interrupt
+  HAL_TIM_IRQHandler(&htim16);
 
-	// TODO: Change to next LED pattern; output 0x01 if the read SPI data is incorrect
-	uint8_t binary_Val = read_from_address(counter);
+  // TODO: Change to next LED pattern; output 0x01 if the read SPI data is incorrect
+//  uint8_t binary_Val = read_from_address(counter);
 
-	if(binary_Val == patterns[count])
-	{
-		GPIOB-> ODR = read_from_address(counter);
-		counter +=1;
-		count +=1;
-		if(counter>5)
-		{
-			counter = 0;
-			count = 0;
-		}
-	}
-	else
-	{
-		GPIOB-> ODR = 0b00000001;
-	}
+
+  if(patterns[counter] == read_from_address(counter) )
+  {
+    GPIOB-> ODR = read_from_address(counter);
+    if(counter==5)
+    {
+      counter = 0;
+    }
+    else
+    {
+      counter +=1;
+    }
+
+  }
+  else
+  {
+    GPIOB->ODR = 0b00000001;
+  }
+
+//    GPIOB-> ODR = read_from_address(counter);
+//    counter +=1;
+//    count +=1;
+//    if(counter>5)
+//    {
+//      counter = 0;
+//      count = 0;
+//    }
+
+//  else
+//  {
+//    GPIOB->ODR = 0b00000001;
+//  }
 
 
 
@@ -498,3 +522,5 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
+
